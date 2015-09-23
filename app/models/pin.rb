d2ae76e95a8bf8cb7edb8ad2281d16de95032b95
Andrej Lukac <zeonvideos@gmail.com>
2015-09-23 20:13:38 +0200

    Forms Centered

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,8 +5,9 @@ class Pin < ActiveRecord::Base
 	validates :description, presence: true
 	validates :user_id, presence: true
 	validates_attachment :image, presence: true,
-  						content_type: { content_type: ["image/jpeg", "image/jpg", "image/gif", "image/png"] },
+  						# content_type: { content_type: ["image/jpeg", "image/jpg", "image/gif", "image/png"] },
   						size: { in: 0..5.megabytes }
+  	validates_attachment_content_type :image, :content_type => /\Aimage/
 
 	belongs_to :user
 	
