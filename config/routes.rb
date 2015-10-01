dd1e62e4f743554ffd716df81c186dabb34a9464
Andrej Lukac <zeonvideos@gmail.com>
2015-10-01 17:23:51 +0200

    Save before renaming pins

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
 
   get 'contact' => 'pages#contact'
 
-  get 'fresh' => 'pages#fresh'
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
